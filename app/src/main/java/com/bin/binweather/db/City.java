1d6f87a685e9acb382f3c556c15a37c56d593ce2
Bin <zb199454@gmail.com>
2018-03-26 14:05:58 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/bin/binweather/db/City.java
@@ -0,0 +1,47 @@
+package com.bin.binweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 123 on 2018/3/26.
+ * 数据库，城市类
+ */
+
+public class City extends DataSupport {
+        private int id;
+        private String cityName;
+        private int cityCode;
+        private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
